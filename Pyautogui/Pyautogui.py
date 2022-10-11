b2260f405ce5ee2d3ea0fbbde9eabeba80ccf44f
ivanpereira2106 <88799861+ivanpereira2106@users.noreply.github.com>
2022-10-11 15:52:31 -0300

    Add files via upload

--- Pyautogui/Pyautogui.py
+++ Pyautogui/Pyautogui.py
@@ -4,7 +4,7 @@ pyautogui.moveTo(0,0,duration=1)
 pyautogui.moveTo(100,200,duration=1)
 #pyautogui.typewrite("Pilin", interval=0.25)
 #pyautogui.move(200,200,duration=1)
-imagen=pyautogui.locateOnScreen(r'C:\Users\ivan.pereira\Documents\Python_Proyects\SAP.png',confidence=0.8)
+imagen=pyautogui.locateOnScreen(r'C:\Proyectos_Python\Pyautogui\SAP.png',confidence=0.8)
 
 if imagen is None:
     print('Image not found on the screen!')
